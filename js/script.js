a7add2c46f249979a94968032a1b19c17ce7c874
James Hooper <jameshooperdesigns@gmail.com>
2021-10-13 15:28:17 -0600

    Add css3 animation fadein for the hero image/title

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+
+gsap.set("body", {autoAlpha:1}) //remove fouc
+const split = new SplitText("h1", {type:"chars"})
+
+const tl = gsap.timeline({repeat:10, id:"Made By Carl"})
+tl.from(split.chars, {duration:1, opacity:0, stagger:0.05, ease:"power2.in"})
+
+tl.to(split.chars, {duration:0.5, opacity:0, stagger:{
+	from:"end",
+	each:0.05
+}}, "+=1")
+
+GSDevTools.create({animation:tl})
